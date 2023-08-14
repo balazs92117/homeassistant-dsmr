9dc2d3fb6c48edcba43684c1b840449fe48e5ff8
root <root@BALAZS-ASUS.localdomain>
2023-08-14 22:44:29 +0200

    test

--- custom_components/dsmr/const.py
+++ custom_components/dsmr/const.py
@@ -31,7 +31,7 @@ DEVICE_NAME_ELECTRICITY = "Electricity Meter"
 DEVICE_NAME_GAS = "Gas Meter"
 DEVICE_NAME_WATER = "Water Meter"
 
-DSMR_VERSIONS = {"2.2", "4", "5", "5B", "5L", "5S", "Q3D"}
+DSMR_VERSIONS = {"2.2", "4", "5", "5B", "5L", "5S", "Q3D", "5EONHU"}
 
 DSMR_PROTOCOL = "dsmr_protocol"
 RFXTRX_DSMR_PROTOCOL = "rfxtrx_dsmr_protocol"
